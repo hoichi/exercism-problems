8184b10030a2b946db5a9b42b028d2d329b9177a
Sergey Samokhov <s.samokhov@aventica.ru>
2018-10-16 18:04:59 +0300

    Finished golfing bracket-push with Simon
    That was awesome :)

--- ocaml/bracket-push/bracket_push.ml
+++ ocaml/bracket-push/bracket_push.ml
@@ -1,43 +1,17 @@
 open Base
+open Base.Continue_or_stop
 
-type bracket =
-  | Left of char
-  | Right of char
-  | Neither
-
-let brackets = [
-  '[',']';
-  '{','}';
-  '(',')';
-]
-
-let map_t2 (a, b) f = (f a, f b)
-
-let bracket_type =
-  let (lefts, rights) = map_t2
-    (List.unzip brackets)
-    (Set.of_list (module Char))
-  in
-  function
-  | c when Set.mem lefts c -> Left c
-  | c when Set.mem rights c -> Right c
-  | _ -> Neither
-
-let brackets_match l r =
-  let matches = Map.of_alist_exn (module Char) brackets in
-  Char.(Map.find_exn matches l = r)
-
-exception Right_bracket_mismatch
-
-let push_or_pop_exn stack c =
-  match bracket_type c, stack with
-  | Neither, s -> s
-  | Left l, s -> l::s
-  | Right r, l::s when brackets_match l r -> s
-  | _ -> raise Right_bracket_mismatch
+let push_or_pop stack c =
+  match stack, c with
+  | _,'[' | _,'{' | _,'(' -> Continue (c::stack)
+  | '['::tl,']' | '{'::tl,'}' | '('::tl,')' -> Continue tl
+  | _,']' | _,'}' | _,')' -> Stop false
+  | stack,_ -> Continue stack
 
 (*  Go through the characters in the string.
     When done, check if some brackets are still open. *)
-let are_balanced s =
-  try String.fold s ~init:[] ~f:(push_or_pop_exn) |> List.is_empty with
-  | _ -> false
+let are_balanced =
+  String.fold_until
+    ~init:[]
+    ~f:push_or_pop
+    ~finish:List.is_empty
