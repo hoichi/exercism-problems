b3865c6d08930637d6e6859360cc106167129548
Sergey Samokhov <s.samokhov@aventica.ru>
2018-10-17 20:08:32 +0300

    Sugar up nucleotide-count

--- ocaml/nucleotide-count/nucleotide_count.ml
+++ ocaml/nucleotide-count/nucleotide_count.ml
@@ -1,5 +1,4 @@
 open Base
-open Base.Result
 
 let check_nucleotide c =
   match c with
@@ -7,25 +6,29 @@ let check_nucleotide c =
   | _ -> Error c
 
 let bump_count m k =
-  let cnt = Map.find m k |> Option.value ~default:0 in
+  let cnt =
+    Map.find m k
+    |> Option.value ~default:0
+  in
   Map.set m ~key:k ~data:(cnt + 1)
 
 let count_nucleotide dna q =
-  check_nucleotide q
-  >>= fun nucl -> String.fold_result dna
-    ~init:0
-    ~f:(fun cnt c ->
-      match check_nucleotide c with
-      | Error c -> Error c
-      | Ok c when Char.(c = nucl) -> Ok (cnt + 1)
-      | _ -> Ok cnt
-    )
+  let open Result in
+  check_nucleotide q >>= fun nucl ->
+    String.fold_result dna
+      ~init:0
+      ~f:(fun cnt c ->
+        check_nucleotide c
+        >>| function
+            | nxt when Char.(nxt = nucl) -> cnt + 1
+            | _ -> cnt
+      )
 
 let count_nucleotides =
+  let open Result in
   String.fold_result
     ~init:(Map.empty (module Char))
     ~f:(fun counts c ->
-      match check_nucleotide c with
-      | Error c -> Error c
-      | Ok nucl -> Ok (bump_count counts nucl)
+      check_nucleotide c
+      >>| bump_count counts
     )
